ba8c8bf1abb5054b540e247b89cd27a5600305d5
fein91 <alexot91@gmail.com>
2015-09-13 20:14:38 +0300

    https://trello.com/c/ZBBPbNAd/ test data added

---
+++ db/test_data.sql
@@ -0,0 +1,239 @@
+INSERT INTO client(id, name)
+    VALUES (1111, 'test client');
+
+INSERT INTO folder(id, client_fk, name)
+    VALUES(2222, 1111, 'test folder1');
+
+INSERT INTO folder(id, parent_fk, client_fk, name)
+    VALUES(2223, 2222, 1111, 'child test folder1');
+
+INSERT INTO folder(id, client_fk, name)
+    VALUES(2232, 1111, 'test folder2');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3333, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3334, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3335, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3336, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3337, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3338, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3339, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3340, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3341, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3342, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3343, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3344, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3345, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3346, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3347, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3348, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3349, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3350, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3351, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3352, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3353, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3354, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3355, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3356, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3357, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3358, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(33343, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(33344, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(33345, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(33346, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(33347, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(33348, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(33349, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(33350, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(33351, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(33352, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(33353, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(33354, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(33355, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(33356, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(33357, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(33358, 'test article1', 2222, 'test url1', 'test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3359, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3360, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3361, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3362, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3363, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3364, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3365, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3366, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3367, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3368, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3369, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3370, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3371, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3372, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3373, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3374, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3375, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3376, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3377, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3378, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3379, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3380, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3381, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3382, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3383, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3384, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3385, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3386, 'child test article1', 2223, 'child test url1', 'child test descr1');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3387, 'test article2', 2232, 'test url2', 'test descr2');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3388, 'test article2', 2232, 'test url2', 'test descr2');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3389, 'test article2', 2232, 'test url2', 'test descr2');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3390, 'test article2', 2232, 'test url2', 'test descr2');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3391, 'test article2', 2232, 'test url2', 'test descr2');
+
+INSERT INTO article(id, name, folder_fk, url, description)
+    VALUES(3392, 'test article2', 2232, 'test url2', 'test descr2');
