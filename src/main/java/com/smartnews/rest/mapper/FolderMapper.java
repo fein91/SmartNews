d1cee0ba79dd75b0bc563c30cb867894fc10aba1
fein91 <alexot91@gmail.com>
2015-09-14 22:56:10 +0300

    FolderMapper refactor

--- src/main/java/com/smartnews/rest/mapper/FolderMapper.java
+++ src/main/java/com/smartnews/rest/mapper/FolderMapper.java
@@ -1,17 +1,12 @@
 package com.smartnews.rest.mapper;
 
 import com.smartnews.model.Folder;
-import com.smartnews.rest.dto.ArticleDto;
 import com.smartnews.rest.dto.FolderDto;
 import com.smartnews.service.FoldersService;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
-
-import java.util.Collections;
-import java.util.List;
 
 @Component
 public class FolderMapper implements RestMapper<FolderDto, Folder> {
@@ -23,13 +18,9 @@ public class FolderMapper implements RestMapper<FolderDto, Folder> {
 
     @Override
     public FolderDto mapToDto(Folder folder) {
-        List<FolderDto> children = !CollectionUtils.isEmpty(folder.getSubFolders())
-                ? mapToDtos(folder.getSubFolders())
-                : Collections.emptyList();
-        List<ArticleDto> articles = foldersService.findArticlesByFolderID(folder.getId(), FoldersService.DEFAULT_PAGE, FoldersService.DEFAULT_PAGE_SIZE);
         return FolderDto.newBuilder(folder.getId(), folder.getName())
-                .children(children)
-                .articles(articles)
+                .children(mapToDtos(folder.getSubFolders()))
+                .articles(foldersService.findArticlesByFolderID(folder.getId(), FoldersService.DEFAULT_PAGE, FoldersService.DEFAULT_PAGE_SIZE))
                 .build();
     }
 
